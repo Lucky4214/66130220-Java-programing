fe13891b0b3d3ae9f22a71781c5f25d2724aa10b
Trần Ngọc Bảo <tranngocbao1122334455@gmail.com>
2026-04-02 00:57:00 +0700

    complete arrayList

--- ArrayList/src/Bao_TN/runMain.java
+++ ArrayList/src/Bao_TN/runMain.java
@@ -23,11 +23,26 @@ public class runMain {
 	}
 		//in ra màn hình danh sách 
 		System.out.println("Danh sách là:");
-		for (int i=0;i<dsSoNguyen.size();i++) {
-			//Lấy ra phần tử thứ i, cất vào biến tạm
-			Integer tam = dsSoNguyen.get(i);
-			System.out.print(tam+" ");
+//		for (int i=0;i<dsSoNguyen.size();i++) {
+//			//Lấy ra phần tử thứ i, cất vào biến tạm
+//			Integer tam = dsSoNguyen.get(i);
+//			System.out.print(tam+" ");
+//		}
+		for(Integer x: dsSoNguyen) {
+			System.out.print(x+" ");
 		}
+		//đếm số phần tử chẵn
+		int soLuongPTchan = 0;
+		for(Integer x: dsSoNguyen) {
+			if(x%2==0) soLuongPTchan =soLuongPTchan+1;
+		}
+		System.out.print("Số lượng phần tử chẵn là:" + soLuongPTchan);
+		//tính tổng các phần tử
+		int TongAll=0;
+		for(Integer x: dsSoNguyen) {
+			TongAll = TongAll +x;
+		}
+		System.out.print("\nTổng tất cả phần tử là:" + TongAll);
 
 	}
 }
